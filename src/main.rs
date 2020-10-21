30f89382fe4a36f09eee781d8cf84a0892b2715f
polonaiz <polonaiz@gmail.com>
2020-10-21 19:10:30 +0900

    test read file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+fn main() {}
+
+#[test]
+fn test_read_file() {
+    use std::io::Read;
+    let path = std::path::Path::new("./src/test.txt");
+    let mut file = std::fs::File::open(path).unwrap();
+    let mut buf = [0; 1024];
+    let size = file.read(&mut buf).unwrap();
+    println!("size: {}", size);
+    println!("buf: {:x?}", &buf[..size]);
 }
