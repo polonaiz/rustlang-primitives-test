c987fb67a2a9a745e1540c5dff385c76d591b246
polonaiz <polonaiz@gmail.com>
2020-10-22 13:19:39 +0900

    move code to testcase

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-fn main() {}
+fn main() {
+}
 
 #[test]
 fn test_read_file() {
@@ -7,6 +8,18 @@ fn test_read_file() {
     let mut file = std::fs::File::open(path).unwrap();
     let mut buf = [0; 1024];
     let size = file.read(&mut buf).unwrap();
+    println!("size: {}", size);
+    println!("buf: {:x?}", &buf[..size]);
+}
+
+#[test]
+fn test_read_file_using_reader() {
+    use std::io::Read;
+    let path = std::path::Path::new("./src/test.txt");
+    let file = std::fs::File::open(path).unwrap();
+    let mut reader = std::io::BufReader::new(file);
+    let mut buf = [0; 1024];
+    let size = reader.read(&mut buf).unwrap();
     println!("size: {}", size);
     println!("buf: {:x?}", &buf[..size]);
 }
